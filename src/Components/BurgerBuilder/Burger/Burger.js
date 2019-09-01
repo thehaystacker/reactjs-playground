293a3211f9033d9867db980758b12c45db6dbb6a
thehaystacker <thehaystacker.in@gmail.com>
2019-09-01 11:38:47 +0530

    Creating burger and its controls

---
+++ src/Components/BurgerBuilder/Burger/Burger.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const burger = () => {
+  return (
+    <div>Burger</div>
+  )
+}
+
+export default burger;
--- src/Components/BurgerBuilder/BurgerBuilder.js
+++ src/Components/BurgerBuilder/BurgerBuilder.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { Component } from "react";
 
-const burgerbuilder = (props) => {
-  return (
-    <div>burgerbuilder</div>
-  )
+import Aux from '../../HOC/Aux.js';
+
+import Burger from './Burger/Burger.js';
+import Controls from './Controls/Controls.js';
+
+class BurgerBuilder extends Component {
+  state = {
+    ingredients: {
+      salad: 0,
+      bacon: 0,
+      seeds: 0,
+      sausage: 0,
+    }
+  }
+  
+  render() {
+    return (
+      <Aux>
+        <Burger ingredients={this.state.ingredients} />
+        <Controls ingredients={this.state.ingredients} />
+      </Aux>
+    );
+  }
 }
 
-export default burgerbuilder;
+export default BurgerBuilder;
