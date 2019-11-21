1977d83735d08931722113fe9880c1b971032589
thehaystacker <thehaystacker.in@gmail.com>
2019-11-21 12:24:33 +0530

    Creating the form elements

--- src/App/App.js
+++ src/App/App.js
@@ -1,107 +1,79 @@
 import React, { Component } from "react";
-import validate from "./Validate";
 
 class App extends Component {
-  state = {
-    formControls: {
-      name: {
-        value: "",
-        placeholder: "Input your name",
-        valid: false,
-        touched: false,
-        validationRules: {
-          minLength: 3
-        }
-      },
-      email: {
-        value: "",
-        placeholder: "Email Address",
-        valid: false,
-        touched: false,
-        validationRules: {
-          isEmail: true
-        }
-      },
-      password: {
-        value: ""
-      }
-    }
-  };
-
-  fnOnchange = event => {
-    let name = event.target.name;
-    let value = event.target.value;
-
-    const updatedControls = { ...this.state.formControls };
-    const updatedFormElement = { ...updatedControls[name] };
-
-    updatedFormElement.value = value;
-    updatedFormElement.touched = true;
-    updatedFormElement.valid = validate(
-      value,
-      updatedFormElement.validationRules
-    );
-
-    updatedControls[name] = updatedFormElement;
-
-    this.setState({
-      formControls: updatedControls
-    });
-  };
-
-  fnFormSubmit = event => {
-    event.preventDefault();
-
-    console.log("[formControls]", this.state.formControls);
-  };
+  state = {};
 
   render() {
-    const { formControls } = this.state;
-
-    let inputNameClass = "form-input";
-    if (formControls.name.touched && !formControls.name.valid) {
-      inputNameClass = "form-input error";
-    }
-
-    let inputEmailClass = "form-input";
-    if (formControls.email.touched && !formControls.email.valid) {
-      inputEmailClass = "form-input error";
-    }
-
     return (
-      <div>
-        <form onSubmit={this.fnFormSubmit}>
-          <label htmlFor="">
-            {formControls.name.valid ? "valid" : "Invalid"}
-          </label>
-
-          <div>
-            <input
-              type="text"
-              placeholder={formControls.name.placeholder}
-              name="name"
-              value={formControls.name.value}
-              onChange={this.fnOnchange}
-              className={inputNameClass}
-            />
+      <div className="form-wrap">
+        <form action="#" method="POST">
+          <div className="frm-group">
+            <label htmlFor="">Textbox</label>
+            <input type="text" className="inp-txt" />
           </div>
 
-          <div>
-            <label htmlFor="">
-              {formControls.email.valid ? "valid" : "Invalid"}
-            </label>
-
-            <input
-              type="email"
-              name="email"
-              placeholder={formControls.email.placeholder}
-              value={formControls.email.value}
-              onChange={this.fnOnchange}
-              className={inputEmailClass}
-            />
+          <div className="frm-group">
+            <label htmlFor="">Textarea</label>
+            <textarea name="" id="" className="inp-textarea"></textarea>
           </div>
 
-          <button type="submit">Submit</button>
+          <div className="frm-group">
+            <label htmlFor="">Checkbox</label>
+            <div className="inp-chk-wrap">
+              <label htmlFor="">
+                <input type="checkbox" name="level" id="" value="beginner" />
+                Beginner
+              </label>
+            </div>
+            <div className="inp-chk-wrap">
+              <label htmlFor="">
+                <input
+                  type="checkbox"
+                  name="level"
+                  id=""
+                  value="intermediate"
+                />
+                Intermediate
+              </label>
+            </div>
+            <div className="inp-chk-wrap">
+              <label htmlFor="">
+                <input type="checkbox" name="level" id="" value="advanced" />
+                Advanced
+              </label>
+            </div>
+          </div>
+
+          <div className="frm-group">
+            <label htmlFor="">Radio</label>
+            <div className="inp-rdo-wrap">
+              <label htmlFor="">
+                <input type="radio" name="gender" value="male" />
+                Male
+              </label>
+            </div>
+            <div className="inp-rdo-wrap">
+              <label htmlFor="">
+                <input type="radio" name="gender" value="female" />
+                Female
+              </label>
+            </div>
+          </div>
+
+          <div className="frm-group">
+            <label htmlFor="">Textbox</label>
+            <select name="options" id="">
+              <option value="option1">Option 1</option>
+              <option value="option2">Option 2</option>
+              <option value="option3">Option 3</option>
+            </select>
+          </div>
+
+          <div className="frm-btn-grp">
+            <button type="submit" className="frm-btn">
+              Submit
+            </button>
+          </div>
         </form>
       </div>
     );
