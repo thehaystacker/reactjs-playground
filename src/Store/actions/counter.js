a8efbbd35908bf032d4b42291c193c23d90b1e42
thehaystacker <thehaystacker.in@gmail.com>
2019-12-20 11:44:20 +0530

    Counter

--- src/Store/actions/counter.js
+++ src/Store/actions/counter.js
@@ -1,11 +1,7 @@
 export const incrementCounter = () => {
-  console.log("[Action] incrementCounter");
-
   return { type: "INCREMENT_COUNTER" };
 };
 
 export const cbIncrementCounterSuccess = () => {
-  console.log("[cbIncrementCounterSuccess]");
-
   return { type: "SUCCESS_INCREMENT_COUNTER" };
 };
