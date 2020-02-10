e1f9ca1db0ee5c33f6d70c62c88daf076f0ac5e1
thehaystacker <thehaystacker.in@gmail.com>
2020-02-10 13:06:15 +0530

    Chapter4 - list and conditionals completed

--- src/App.js
+++ src/App.js
@@ -3,21 +3,58 @@ import "./App.css";
 import Person from "./components/person";
 
 class App extends Component {
-  fnInputOnchange = (event, value) => {
-    console.log("[event]", event.target.value);
-    console.log("[value]", value);
+  state = {
+    persons: [
+      { name: "Person 1", role: "Role 1" },
+      { name: "Person 2", role: "Role 2" },
+      { name: "Person 3", role: "Role 3" }
+    ],
+    showPersons: true
   };
+
+  cbDeletePerson = idx => {
+    console.log(idx);
+
+    let persons = [...this.state.persons];
+    persons.splice(idx, 1);
+
+    console.log(persons);
+    
+    this.setState({ persons: persons });
+  };
+
+  cbInputOnchange = (event, idx) => {
+    let persons = [...this.state.persons];
+    persons[idx].name = event.target.value;
+    this.setState({persons});
+  };
+
   render() {
+    let persons = null;
+    if (this.state.persons) {
+      persons = (
+        <div className="wrap-persons">
+          {this.state.persons.map((person, idx) => {
+            return (
+              <Person
+                name={person.name}
+                role={person.role}
+                key={idx}
+                idx={idx}
+                cbInputOnchange={this.cbInputOnchange}
+                cbDeletePerson={this.cbDeletePerson}
+              />
+            );
+          })}
+        </div>
+      );
+    }
+
     return (
       <div className="App">
         <h1>Hello world</h1>
 
-        <Person />
-        <Person
-          cbInputOnchange={(event) => this.fnInputOnchange(event, 'somevalue')}
-          defaultValue="sangeeth"
-        />
-        <Person />
+        {persons}
       </div>
     );
   }
--- src/components/person.js
+++ src/components/person.js
@@ -3,15 +3,19 @@ import React from "react";
 const Person = props => {
   return (
     <div className="cmp-person">
-      <h1>Hi!, I'm a person</h1>
+      <h1>
+        Hi!, my name is {props.name} and I'm a {props.role}
+      </h1>
 
       <input
         type="text"
-        name=""
-        id=""
-        onChange={props.cbInputOnchange}
-        defaultValue={props.defaultValue}
+        onChange={event => props.cbInputOnchange(event, props.idx)}
+        value={props.name}
       />
+
+      <button type="button" onClick={() => props.cbDeletePerson(props.idx)}>
+        Delete Me
+      </button>
     </div>
   );
 };
